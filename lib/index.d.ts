f9255a54e8c2dc66fdd40ff4f8d67bbb31463f16
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2019-08-15 21:27:07 +0200

    Fix

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,11 @@
 import { Plugin } from 'webpack'
 
-export = HTMLMinifierPlugin;
-
-declare class HTMLMinifierPlugin extends Plugin {
-  constructor (minifierOptions?: object, options?: HTMLMinifierPlugin.Options);
-}
-
 declare namespace HTMLMinifierPlugin {
   interface Options {
     verbose?: boolean
   }
+}
+
+export default class HTMLMinifierPlugin extends Plugin {
+  constructor (minifierOptions?: object, options?: HTMLMinifierPlugin.Options)
 }
